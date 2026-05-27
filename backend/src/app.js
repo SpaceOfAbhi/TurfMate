70934fcf06f94210adc1e17a1b8985d6cd981724
SpaceOfAbhi <abhinav522005@gmail.com>
2026-05-27 18:11:34 +0530

    Auth backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
-
+import authRoutes from "./routes/auth.routes.js";
 import matchRoutes from "./routes/match.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.use("/api/matches", matchRoutes);
 
 app.get("/", (req, res) => {
