0ec5cdd0fa3b5ddad394e2b5535aa761701ad9ab
SpaceOfAbhi <abhinav522005@gmail.com>
2026-05-29 12:49:32 +0530

    auth backend

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,9 @@
 import express from "express";
-
-import {
-  googleLogin,
-} from "../controllers/auth.controller.js";
+import { signup, login } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/google", googleLogin);
+router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
