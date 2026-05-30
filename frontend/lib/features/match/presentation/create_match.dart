34a8779d88c787a6c705ca5897ba3fa04768d026
SpaceOfAbhi <abhinav522005@gmail.com>
2026-05-30 10:42:43 +0530

    start/end time updated

--- frontend/lib/features/match/presentation/create_match.dart
+++ frontend/lib/features/match/presentation/create_match.dart
@@ -19,9 +19,17 @@ class _CreateMatchScreenState extends State<CreateMatchScreen> {
   final slotsController = TextEditingController();
 
   final amountController = TextEditingController();
+  final dateController = TextEditingController();
+  final timeController = TextEditingController();
 
   final MatchService matchService = MatchService();
 
+  DateTime? selectedDate;
+  TimeOfDay? selectedTime;
+
+  final durations = [60, 90, 120];
+
+  int selectedDuration = 60;
   bool isLoading = false;
 
   Future<void> createMatch() async {
@@ -29,14 +37,47 @@ class _CreateMatchScreenState extends State<CreateMatchScreen> {
       return;
     }
 
+    if (selectedDate == null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Select a date")));
+
+      return;
+    }
+
+    if (selectedTime == null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Select a time")));
+
+      return;
+    }
+
     setState(() {
       isLoading = true;
     });
 
+    final startTime = DateTime(
+      selectedDate!.year,
+      selectedDate!.month,
+      selectedDate!.day,
+      selectedTime!.hour,
+      selectedTime!.minute,
+    );
+
+    final endTime = startTime.add(Duration(minutes: selectedDuration));
+
     final success = await matchService.createMatch(
       sport: sportController.text,
+
       turfName: turfController.text,
+
+      startTime: startTime.toIso8601String(),
+
+      endTime: endTime.toIso8601String(),
+
       totalSlots: int.parse(slotsController.text),
+
       amountPerPerson: double.parse(amountController.text),
     );
 
@@ -68,6 +109,8 @@ class _CreateMatchScreenState extends State<CreateMatchScreen> {
     turfController.dispose();
     slotsController.dispose();
     amountController.dispose();
+    dateController.dispose();
+    timeController.dispose();
 
     super.dispose();
   }
@@ -116,6 +159,94 @@ class _CreateMatchScreenState extends State<CreateMatchScreen> {
               ),
 
               const SizedBox(height: 16),
+              TextFormField(
+                controller: dateController,
+                readOnly: true,
+
+                decoration: const InputDecoration(
+                  //  labelText: "Match Date",
+                  hintText: "Select Match Date",
+                  prefixIcon: Icon(Icons.calendar_today),
+                ),
+
+                onTap: () async {
+                  final date = await showDatePicker(
+                    context: context,
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime.now().add(const Duration(days: 60)),
+                    initialDate: DateTime.now(),
+                  );
+
+                  if (date != null) {
+                    selectedDate = date;
+
+                    dateController.text =
+                        "${date.day}/${date.month}/${date.year}";
+                  }
+                },
+
+                validator: (_) {
+                  if (selectedDate == null) {
+                    return "Select a date";
+                  }
+
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: timeController,
+                readOnly: true,
+
+                decoration: const InputDecoration(
+                  labelText: "Start Time",
+                  prefixIcon: Icon(Icons.access_time),
+                ),
+
+                onTap: () async {
+                  final time = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.now(),
+                  );
+
+                  if (time != null) {
+                    selectedTime = time;
+
+                    timeController.text = time.format(context);
+                  }
+                },
+
+                validator: (_) {
+                  if (selectedTime == null) {
+                    return "Select a time";
+                  }
+
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+
+              DropdownButtonFormField<int>(
+                value: selectedDuration,
+
+                decoration: const InputDecoration(labelText: "Duration"),
+
+                items: const [
+                  DropdownMenuItem(value: 60, child: Text("1 Hour")),
+
+                  DropdownMenuItem(value: 90, child: Text("1.5 Hours")),
+
+                  DropdownMenuItem(value: 120, child: Text("2 Hours")),
+                ],
+
+                onChanged: (value) {
+                  setState(() {
+                    selectedDuration = value!;
+                  });
+                },
+              ),
+
+              const SizedBox(height: 16),
 
               TextFormField(
                 controller: slotsController,
@@ -152,7 +283,6 @@ class _CreateMatchScreenState extends State<CreateMatchScreen> {
                   return null;
                 },
               ),
-
               const SizedBox(height: 24),
 
               SizedBox(
