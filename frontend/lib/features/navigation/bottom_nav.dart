c7ce9cff6eb33f5bdadfe486c963c3c1f77f9a81
SpaceOfAbhi <abhinav522005@gmail.com>
2026-05-31 22:12:41 +0530

    bottom nav

--- frontend/lib/features/navigation/bottom_nav.dart
+++ frontend/lib/features/navigation/bottom_nav.dart
@@ -1,67 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/core/theme/colors.dart';
 import 'package:frontend/features/match/presentation/create_match.dart';
 import 'package:frontend/features/match/presentation/my_matches.dart';
 
 import '../home/presentation/home_screen.dart';
 
-
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
-  State<MainNavigationScreen> createState() =>
-      _MainNavigationScreenState();
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
 }
 
-class _MainNavigationScreenState
-    extends State<MainNavigationScreen> {
-
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int currentIndex = 0;
 
-  final screens = const [
-    HomeScreen(),
-    CreateMatchScreen(),
-    MyMatchesScreen(),
-  ];
+  final screens = const [HomeScreen(), CreateMatchScreen(), MyMatchesScreen()];
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      
-
+      extendBody: true,
       body: screens[currentIndex],
 
-      bottomNavigationBar:
-          BottomNavigationBar(
-         backgroundColor: Colors.transparent,
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+    
+        decoration: BoxDecoration(
+          color:AppColors.backgroundColor,
 
-        currentIndex: currentIndex,
+          borderRadius: BorderRadius.circular(30),
 
-        onTap: (index) {
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.focusColor.withOpacity(0.5),
 
-          setState(() {
-            currentIndex = index;
-          });
-        },
-
-        items: const [
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle),
-            label: "Create",
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.sports_soccer),
-            label: "My Matches",
-          ),
-        ],
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          
+          backgroundColor:Colors.transparent,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: AppColors.focusColor,
+          currentIndex: currentIndex,
+          iconSize: 30,
+          selectedFontSize: 14,
+          unselectedFontSize: 12,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle),
+              label: "Create",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.sports_soccer),
+              label: "My Matches",
+            ),
+          ],
+        ),
       ),
     );
   }
